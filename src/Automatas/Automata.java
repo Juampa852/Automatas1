5be1da9883aedd2923eb834461d4ff77ac5dee34
juampa <juampa@localhost>
2017-02-02 12:49:34 -0600

    Creadas todas las clases, con sus atributos básicos

--- src/Automatas/Automata.java
+++ src/Automatas/Automata.java
@@ -3,12 +3,16 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package automatas.proyecto.pkg1;
+package Automatas;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author juampa
  */
 public class Automata {
-    
+    ArrayList<String> lenguaje=new ArrayList<String>();
+    Estado inicial;
+    ArrayList<Estado> estados=new ArrayList<Estado>();
 }
