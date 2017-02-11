40503f419b84828a55a2691f50824025c26ba965
Roberto Arriola <arriola.rock97@gmail.com>
2017-02-11 14:16:23 -0600

    *Clase privada estadoG ya no hereda de Estado. *Clase privada estadOG tiene como otributo un onjeto de tipo Estado *Inclusion del metodo dibujarEstado

--- src/Automatas/Dibujar.java
+++ src/Automatas/Dibujar.java
@@ -6,15 +6,15 @@
 package Automatas;
 
 import Excepciones.EstadoNoExiste;
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Label;
+import java.awt.Point;
 import java.awt.RenderingHints;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**Clase que hereda de JPanel
@@ -22,52 +22,81 @@ import javax.swing.JPanel;
  * @author Roberto Arriola
  */
 public class Dibujar extends JPanel{
-    JFrame ventana= new JFrame("Automata");//Se crea un nuevo frame
-    Automata automataPrueba=new Automata();
+    private JFrame ventana= new JFrame("Automata");//Se crea un nuevo frame
+    private Automata automataPrueba=new Automata();
+    private ArrayList<estadoG> estados=new ArrayList<>();
     /**
      * Constructor de la clase que dibuja el automata
+     * Genera un arreglo de estados graficos marcando en la primera posicion el estado inicial
      * @param auto automata a dibujar
+     * @throws Excepciones.EstadoNoExiste
      */
-    public Dibujar(Automata auto)
+    public Dibujar(Automata auto) throws EstadoNoExiste
     {
-        ventana.setSize(400, 300);//Setea el tamaño del lienzo
-        ventana.setVisible(true);//Hace visible todo el frame
-        ventana.add(this);//Añade el lienzo al frame
-        
+              
+            automataPrueba=auto;
+            ventana.setSize(400, 300);//Setea el tamaño del lienzo
+            ventana.setVisible(true);//Hace visible todo el frame
+            ventana.add(this);//Añade el lienzo al frame
+            int i=auto.getPocEstadoInicial();
+            if(i>-1)
+            {
+                estadoG ini= new estadoG(auto.getEstado(auto.getPocEstadoInicial()),100, 100);
+                estadoG est;
+                estados.add(ini);
+                for(int o=0;o<auto.noEstados();o++)
+                {
+                    est = new estadoG(auto.getEstado(o),50*o+100, 100);
+                    if(!ini.est.getNombre().equals(est.est.getNombre()))
+                        estados.add(ini);
+                }
+            }
     }
     @Override
     public void paint(Graphics g)
     {
         super.paint(g);
-        this.repaint();
-        Graphics2D g2d= (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        int pos=automataPrueba.getPocEstadoInicial();
-        try {
-            estadoG estG=new estadoG(automataPrueba.getEstado(pos),100,100);
-            estG.label.setLocation(100,100);
-        } catch (EstadoNoExiste ex) {
-            Logger.getLogger(Dibujar.class.getName()).log(Level.SEVERE, null, ex);
+        for(int i=0;i<estados.size();i++)
+        {
+            dibujarEstado(i,estados.get(i),g);
         }
         
-        g2d.drawOval(100,100,100,100);
-        /*int menor;
-        if(this.getWidth()<this.getHeight())
-            menor=this.getWidth();
-        else 
-            menor=this.getHeight();
-        g2d.drawOval(this.getWidth()/10,this.getHeight()/2-menor/4,menor/2,menor/2);*/
     }
+    /**
+     * Metodo que dibuja en el panel un estado grafico, una flecha si este es inicial,un doble circulo si es final
+     * @param posicion posicion que tiene el estado grafico en el arreglo de la clase
+     * @param estG el objeto estado grafico a dibujar
+     * @param g objeto de tipo graphics que permite realizar dibujos en el panel
+     */
+    public void dibujarEstado(int posicion, estadoG estG,Graphics g){
+        Graphics2D g2d= (Graphics2D) g;
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        if(posicion==0)
+        {
+            g2d.drawLine(estG.x-30,estG.y+20, estG.x,estG.y+50);
+            g2d.drawLine(estG.x-30,estG.y+80, estG.x,estG.y+50);
+            g2d.drawOval(estG.x,estG.y,100,100);
+            if(estG.est.isFinal())
+                g2d.drawOval(estG.x+10,estG.y+10,80,80);
+            g2d.drawChars(estG.est.getNombre().toCharArray(), 0,estG.est.getNombre().length() ,estG.x+50,estG.y+50);
+                
+        }
+            
+        
+    }
+    /**
+     * Clase estado grafico, es una clase interna de la clase Dibujar
+     * Su funcion es almacenar un estado y la posicion que se le dara en el panel
+     * de esa forma, al graficar una transicion, puede conocerse las pocisiones de los estados en el lienzo
+     */
     private class estadoG{
         int x, y;
-        Estado est, siguiente=null;
-        Label label=new Label();
+        Estado est;
         public estadoG(Estado est, int x, int y)
         {
             this.est=est;
             this.x=x;
             this.y=y;
-            label.setText(est.getNombre());
         }
     }
 }
