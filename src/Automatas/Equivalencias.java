6e643a1f2f2ab7d6d1ea798e3223d7e9e5b97e23
Juan Miguel <juanmidlc@gmail.com>
2017-02-15 09:31:39 -0600

    Ron se chingó el proyecto :((((((((

--- src/Automatas/Equivalencias.java
+++ src/Automatas/Equivalencias.java
@@ -5,13 +5,28 @@
  */
 package Automatas;
 
+import Excepciones.EstadoNoExiste;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 /**
  *
  * @author Lenovo
  */
 public class Equivalencias {
-    
+    public void equivalente (Automata auto1, Automata  auto2){
+        try {
+            if (auto1.getEstado(0).isFinal()==auto2.getEstado(0).isFinal()) {
+                for (int i = 0; i < auto1.getLenguaje().size(); i++) {
+                    EstadosHijos(auto1.getEstado(0).getTransiciones(i),auto2.getEstado(0).getTransiciones(i));
+                }
+                            }
+        } catch (EstadoNoExiste ex) {
+            Logger.getLogger(Equivalencias.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+  
     
     
     
