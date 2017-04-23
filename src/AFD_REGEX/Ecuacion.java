eb8f0cbade61dde617da883ab5c51ab0a335231e
robertoar14 <arriola.rock97@gmail.com>
2017-04-23 12:40:46 -0600

    Proyecto Automatas PARTE 2 *Inclusion de metodos sustituir. agrupar y lemaDeArden en la clase Ecuacion *Inclusion de la funcion generarExpresion en clase ExpresionRegular ----AVANCE------------------------------------------------------------- La clase ExpresionRegular permite generar las ecuaciones del Lema de Arden para un automata, sustituye una dentro de otra desde el  estado final hasta el inicial, y genera la expresion regular del AFD

--- src/AFD_REGEX/Ecuacion.java
+++ src/AFD_REGEX/Ecuacion.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 public class Ecuacion{
     private ArrayList<Termino>terminos=new ArrayList<Termino>();
     private String nombre="";
+    public int noTerminos=0;
     /**
      * Constructor de la clase ecuacion
      * asigna el nombre del estado que esta representando
@@ -31,10 +32,12 @@ public class Ecuacion{
     public void añadirTermino(Termino term)
     {
         terminos.add(term);
+        noTerminos=terminos.size();
     }
     public void removerTermino(Termino term)
     {
         terminos.remove(term);
+        noTerminos=terminos.size();
     }
 
     @Override
@@ -57,16 +60,129 @@ public class Ecuacion{
         for(int i=0;i<terminos.size();i++){//Se recorre todos los terminos de la ecuacio para buscar terminos con el mismo estado
             for(int o=i+1;o<terminos.size();o++)//luego se recorren todos los terminos, a partir del siguiente del que se esta anlizando
             {
-                if(terminos.get(i).getEstado().equals(terminos.get(o).getEstado()))//SI el nombre del estado oincide, pueden agruparse
-                {
-                    terminos.get(i).setRegex(terminos.get(i).getRegex()+"+"+terminos.get(o).getRegex());//se suman las expresiones del estado que se analiza, 
-                    //y de los que se encuentra
-                    terminos.remove(terminos.get(o));//Se remueve el estado que se encontro
-                    o--;//al reducirse el tamaño del Array, se retrocede una posicion del iterador
+                if(terminos.get(i).getEstado()!=null && terminos.get(o).getEstado()!=null){
+                    if(terminos.get(i).getEstado().equals(terminos.get(o).getEstado()))//SI el nombre del estado oincide, pueden agruparse
+                    {
+                        terminos.get(i).setRegex(terminos.get(i).getRegex()+"+"+terminos.get(o).getRegex());//se suman las expresiones del estado que se analiza, 
+                        //y de los que se encuentra
+                        terminos.remove(terminos.get(o));//Se remueve el estado que se encontro
+                        o--;//al reducirse el tamaño del Array, se retrocede una posicion del iterador
+                    }
                 }
             }
             if(terminos.get(i).getRegex().length()>1)//En el caso de que la expresion tenga mas de un caracter se añaden parentesis
-                terminos.get(i).setRegex("("+terminos.get(i).getRegex()+")");
+            {
+                if(terminos.get(i).getRegex().charAt(0)!='(' && terminos.get(i).getRegex().charAt(terminos.get(i).getRegex().length()-1)!=')')
+                    terminos.get(i).setRegex("("+terminos.get(i).getRegex()+")");
+            }
+            noTerminos=terminos.size();
         }
+    }
+    /**
+     * Metodo que aplica el Lema de Arden en una ecuacion.
+     * Para una ecuacion A=sA+r -> A=s*r
+     * Donde A es un estado, a y r son expresiones regulares
+     */
+    public void lemaDeArden()
+    {
+        String expresion="";
+        for(int i=0;i<terminos.size();i++)
+        {            
+            if(terminos.get(i).getEstado()!=null)
+            {
+                if(terminos.get(i).getEstado().equals(nombre))
+                {
+                    expresion=terminos.get(i).getRegex();
+                    if(expresion.charAt(0)!='(' || expresion.charAt(expresion.length()-1)!=')')
+                        expresion="("+expresion+")";
+                    expresion+="*";
+                        
+                    terminos.remove(terminos.get(i));
+                    break;
+                }
+            }
+        }
+        for(Termino term:terminos)
+        {
+            term.setRegex(expresion+term.getRegex());
+        }
+        noTerminos=terminos.size();
+    }
+    /**
+     * Metodo que elimina los terminos cuyo estado tiene el nombre del parametro recibido
+     * @param nombre nombre de estado que se desea eliminar
+     */
+    public void removerEcuacion(String nombre)
+    {
+        for(int i=0;i<terminos.size();i++)//recorre todos los terminos de la ecuacion
+        {
+                if(terminos.get(i).getEstado()!=null)
+                {
+                    if(terminos.get(i).getEstado().equals(nombre))/*si se encuentra una coincidencia entre el estado del termino y el 
+                    nombre de estado, se elimina*/
+                        removerTermino(terminos.get(i));
+                }
+        }
+    }
+    /**
+     * Metodo que sustituye una ecuacion en la actual.
+     * Recorre todos los terminos buscando las coincidencia y al encontrarlos concatena sus expresiones y reemplaza
+     * el estado con el estado de la ecuacion de sustitucion.
+     * Por ejemplo: q0=(a+b)q1+cq2  y   q1=bq0 -> q0=((a+b)b)q0+cq2;
+     * Luego se agrupan los terminos con el metodo agrupar()
+     * @param ecua Ecuacion que se va a sustituir en la actual
+     */
+    public void sustituir(Ecuacion ecua)
+    {
+        System.out.println("/////////////SUSTITUCION////////");
+        System.out.println("SUSTITUIR "+ecua+"   EN "+this);
+        for(int actual=0;actual<terminos.size();actual++)//Recorre todos los terminos de la ecuacion actual
+        {
+            if(terminos.get(actual).getEstado()!=null)
+            {
+                if(terminos.get(actual).getEstado().equals(ecua.nombre))
+                {
+                    System.out.println("    TERMINO: "+terminos.get(actual));
+                    for(int term=0;term<ecua.noTerminos;term++)//si encuentra coincidencia
+                    //añade todos los terminos de la ecuacion de sustitucion y les concatena al inicio de su expresion
+                    //la expresion del termino de la ecuacion actual que se esta analizando
+                    {
+                        if(ecua.terminos.get(term).getEstado()!=null)
+                        //si el estado del termino es diferente de nulo se añade un termino normal
+                        {
+                            añadirTermino(new Termino(ecua.terminos.get(term).getEstado(),terminos.get(actual).getRegex()+ecua.terminos.get(term).getRegex()));
+                            System.out.println("    SE AÑADIO: "+terminos.get(terminos.size()-1)+" en"+nombre);
+                        }
+                        //si es nulo se añade un termino de cadena vacia
+                        else
+                        {
+                            añadirTermino(new Termino(terminos.get(actual).getRegex()+ecua.terminos.get(term).getRegex()));
+                            System.out.println("    SE AÑADIO: "+terminos.get(terminos.size()-1)+" en"+nombre);
+                        }
+                    }
+                    //se remueve el termino de la ecuacion actual
+                        System.out.println("    SE REMOVIO: "+terminos.get(actual)+" en"+nombre);
+                        removerTermino(terminos.get(actual));
+                    break;
+                }
+            }
+        }
+        System.out.println("    ECUACION RESULTANTE "+this);
+        agrupar();
+        System.out.println("    AGRUPADA "+this);
+    }
+    public String expresionEcuacion()
+    {
+        String expresion="";
+        for(Termino term:terminos)
+        {
+            if(term.getEstado()==null)
+            {
+                if(!"".equals(expresion))
+                    expresion+="+";
+                expresion+=term.getRegex();
+            }
+        }
+        return expresion;
     }
 }
--- src/AFD_REGEX/ExpresionRegular.java
+++ src/AFD_REGEX/ExpresionRegular.java
@@ -32,11 +32,13 @@ public class ExpresionRegular {
      * @return Expresion regular en una cadena
      * @throws EstadoNoExiste Excepcion cuando se busca un estado que no existe
      */
-    public void generarExpresion(Automata auto) throws EstadoNoExiste
+    public String generarExpresion(Automata auto) throws EstadoNoExiste
     {
-        String expresion="";
-        Estado estado;
-        for(int i=0;i<auto.noEstados();i++)//Se hace un ciclo que recorre el numero de estados del automata 
+        registro.clear();
+        ecuaciones.clear();
+        registro.clear();
+        crearEcuaciones(auto,auto.getPocEstadoInicial());
+        /*for(int i=0;i<auto.noEstados();i++)//Se hace un ciclo que recorre el numero de estados del automata 
         {    
             estado=auto.getEstado(i);//Se guarda el estado que se esta analizando
             registro.add(estado.getNombre());
@@ -46,20 +48,103 @@ public class ExpresionRegular {
                 Transicion trans=estado.getTransiciones().get(o);//Se guarda cada transicion
                 ecuaciones.get(i).añadirTermino(new Termino(trans.getSiguiente(),trans.getLetra()+""));/*se añaden terminos
                 ala ecuacion, enviando el simbolo de la transicion y el nombre del estado al que conduce
-                */   
+                  
             }
             if(estado.isFinal())
                 ecuaciones.get(i).añadirTermino(new Termino(""));//Si el estado es final se añade una transicion con cadena vacia
+        }*/
+        for(int i=registro.size()-1;i>=0;i--)//recorre todos los registros desde el ultimo
+        {
+            System.out.println("****************************");
+            int posicion=buscarEcuacion(registro.get(i));
+            System.out.println("REGISTRO: "+i+"  NOMBRE: "+registro.get(i)+"  POSICION "+posicion);
+            System.out.println("ECUACION "+ecuaciones.get(posicion));
+            ecuaciones.get(posicion).lemaDeArden();//aplica el lema de arden sobre cada ecuacion
+            System.out.println("LEMMA DE ARDEN "+ecuaciones.get(posicion)+"  Numero Terminos"+ecuaciones.get(posicion).noTerminos);
+            if(ecuaciones.get(posicion).noTerminos<1)/*Si la ecuacion queda sin terminos por el lema de arden 
+                significa que es un estado sumidero, es decir, un estado no final del cual no se puede salir
+                por lo que la expresion regular no lo toma en cuenta
+                */
+            {
+                System.out.println("SE REMOVIO DE ECUACIONES: "+ecuaciones.get(posicion));
+                ecuaciones.remove(ecuaciones.get(posicion));//Remueve el estado sumidero del array de ecuaciones, pero no del registro
+            }
+            if(i<registro.size()-1)//Si la ecuacion que se esta analizando no es la ultima del registro
+                //se recorren todas las posiciones del registro por debajo del actual y se sustituyen las ecuaciones en la actuak
+            {
+                for(int o=i+1;o<registro.size();o++)
+                {
+                    System.out.println("REGISTRO: "+o+"   NOMBRE: "+registro.get(o));
+                    int posicionSustitucion=buscarEcuacion(registro.get(o));//se busca la ecuacion de sustitucion del registro dentro del ARRAY de ecuaciones
+                    System.out.println("Posicion de sustitucion en Ecuaciones: "+posicionSustitucion);
+                    if(posicionSustitucion>-1)//si la posicion es -1 quiere decir que la ecuacion ya no existe en el array
+                        ecuaciones.get(posicion).sustituir(ecuaciones.get(posicionSustitucion));//si existe se sustituye dentro de la ecuacion
+                    else
+                        ecuaciones.get(posicion).removerEcuacion(registro.get(o));//si no existe, se elimina los terminos que lo contengan
+                }
+            }
+            System.out.println("*********************************");
         }
-        //return expresion
+        return ecuaciones.get(0).expresionEcuacion();
     }
-    public boolean existeEnRegistro(String nombre)
+    /**
+     * Metodo recursivo que genera las ecuaciones para los estados si estos no se han añadido a registros
+     * Recorre todas las transiciones de cada estado para generarlas 
+     * @param auto automata
+     * @param i posicion del estado dentro del Array de Estados del Automata
+     * @throws EstadoNoExiste 
+     */
+    private void crearEcuaciones(Automata auto, int i) throws EstadoNoExiste
     {
-        for (Ecuacion ecuacione : ecuaciones) {
-            if(ecuacione.getNombre().equals(nombre))
-                return true;
-        }
-        return false;
+        Estado estado=auto.getEstado(i);//Se guarda el estado que se esta analizando
+        registro.add(estado.getNombre());
+        int indexEcuacion;              
+        ecuaciones.add(new Ecuacion(estado.getNombre()));//Se añade una nueva ecuacion unicamente con el nombre
+        indexEcuacion=ecuaciones.size()-1;
+        System.out.println("Estado de registro "+estado.getNombre()+"  i: "+i+"  indexEcuacion "+indexEcuacion);
+        for(int o=0;o<auto.noLetras();o++)//Ciclo que recorre el lenguaje del automata para genera los terminos de la ecuacion
+        {
+            Transicion trans=estado.getTransiciones().get(o);//Se guarda cada transicion
+            ecuaciones.get(indexEcuacion).añadirTermino(new Termino(trans.getSiguiente(),trans.getLetra()+""));/*se añaden terminos
+            ala ecuacion, enviando el simbolo de la transicion y el nombre del estado al que conduce
+            */   
+            if(!existeEnRegistro(trans.getSiguiente()))//se comprueba si el estado de la transicion existe, si no existe se llama de nuevo al metodo
+                        crearEcuaciones(auto,auto.getPocEstado(trans.getSiguiente()));
+            }
+            if(estado.isFinal())
+            ecuaciones.get(indexEcuacion).añadirTermino(new Termino(""));//Si el estado es final se añade una transicion con cadena vacia
+            System.out.println("ECUACION "+ecuaciones.get(indexEcuacion));
+            ecuaciones.get(indexEcuacion).agrupar();//se agrupa los terminos que tengan el mismo estado
+            System.out.println("AGRUPADA "+ecuaciones.get(indexEcuacion));
+            System.out.println("------------------------------------------------");
     }
-    
+    /**
+     * Funcion booleana que comprueba si un estado se encuentra en el registro
+     * @param nombre nombre del estado que se busca
+     * @return TRUE si el estado si esta en el registro - FALSE si el estado no se ha añadido al registro
+     */
+    private boolean existeEnRegistro(String nombre)
+    {
+        boolean existe=false;
+        for (String registro1 : registro) 
+        {
+            if(registro1.equals(nombre))
+                existe=true;
+        }
+        return existe;
+    }
+    /**
+     * Metodo que busca la posicion de una ecuacion dentro del Array de ecuaciones
+     * @param nombre Nombre de la ecuacion que se busca
+     * @return (-1) si no esta dentro del array  -  (numero>-1) la posicion en el Array
+     */
+    private int buscarEcuacion(String nombre)
+    {
+        for(int i=0;i<ecuaciones.size();i++)
+        {
+            if(ecuaciones.get(i).getNombre().equals(nombre))
+                return i;
+        }
+        return -1;
+    }
 }
